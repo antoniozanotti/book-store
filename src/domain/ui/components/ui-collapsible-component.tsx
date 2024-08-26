19ea7da230c9eb0dfdc42be8998d62ad38c3711c
Antônio Zanotti <antoniorzanotti@gmail.com>
2024-08-26 16:44:54 -0300

    feat: improve menu accessibility

--- src/domain/ui/components/ui-collapsible-component.tsx
+++ src/domain/ui/components/ui-collapsible-component.tsx
@@ -1,25 +1,25 @@
 "use client";
 
 import { ChevronDownIcon, ChevronLeft } from "lucide-react";
-import { useState, ReactNode } from "react";
-import { useOutsideClick } from '../helpers/use-outside-click';
+import { useState, ReactNode, useRef } from "react";
 
-export interface UiCollapsibleComponentInterface extends React.ComponentProps<"div"> {
+export interface UiCollapsibleComponentInterface
+  extends React.ComponentProps<"div"> {
   shouldCloseWhenOutside: boolean;
   theHeader: ReactNode;
   theContent: ReactNode;
 }
 
-export function UiCollapsibleComponent({ shouldCloseWhenOutside, theHeader, theContent, ...props }: UiCollapsibleComponentInterface) {
+export function UiCollapsibleComponent({
+  shouldCloseWhenOutside,
+  theHeader,
+  theContent,
+  ...props
+}: UiCollapsibleComponentInterface) {
   const [isOpen, setIsOpen] = useState(false);
+  const timeoutRef = useRef<number | null>(null);
 
-  const ref = useOutsideClick(() => {
-    if(shouldCloseWhenOutside){
-      setIsOpen(false);
-    }
-  });
-
-  function swapIsOpen() {
+  function onClickHandler() {
     if (isOpen) {
       setIsOpen(false);
     } else {
@@ -27,14 +27,36 @@ export function UiCollapsibleComponent({ shouldCloseWhenOutside, theHeader, theC
     }
   }
 
+  function onFocusHandler() {
+    if (timeoutRef.current !== null) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  function onBlurHandler() {
+    timeoutRef.current = window.setTimeout(() => {
+      setIsOpen(false);
+    });
+  }
+
   const contentClasses = isOpen ? "lg:absolute lg:z-[100]" : "hidden";
 
   return (
-    <div {...props} ref={ref} onClick={swapIsOpen}>
-      <div className="flex cursor-pointer gap-2 p-4 lg:px-10 lg:py-2 items-center">
+    <div {...props} onBlur={onBlurHandler} onFocus={onFocusHandler}>
+      <button
+        className="flex cursor-pointer gap-2 p-4 lg:px-10 lg:py-2 items-center"
+        onClick={onClickHandler}
+        tabIndex={0}
+        aria-haspopup="true"
+        aria-expanded={isOpen}
+      >
         <span className="grow">{theHeader}</span>
-        {isOpen ? <ChevronDownIcon className="size-4" /> : <ChevronLeft className="size-4" />}
-      </div>
+        {isOpen ? (
+          <ChevronDownIcon className="size-4" />
+        ) : (
+          <ChevronLeft className="size-4" />
+        )}
+      </button>
       <div className={contentClasses}>{theContent}</div>
     </div>
   );
